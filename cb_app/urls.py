ce70cb98926f952c3a9ff340599b5b2d86613e5f
sarunkumar1729 <sarunkumarvv1993@gmail.com>
2023-12-01 11:11:46 +0530

    first commit

---
+++ cb_app/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('courses', views.courses, name='courses'),
+    path('contact_us', views.contact_us, name='contact_us'),
+    path('sign_up', views.sign_up, name='sign_up'),
+    path('login', views.user_login, name='login'),
+    path('python', views.python, name='python'),
+    path('adminindex',views.admin_index,name='adminindex'),
+    path('adminlogin',views.admin_login,name='adminlogin'),
+    path('jobs',views.jobs,name='jobs'),
+    path('editprofile',views.edit_profile,name='editprofile'),
+    path('profile',views.profile,name='profile'),
+    path('candidates',views.users_list,name='candidates'),
+    path('userlogout',views.user_logout,name='userlogout'),
+    path('jobsavailable',views.jobs_available,name='jobsavailable'),
+    path('applyjob/<int:i>',views.apply_job,name='applyjob'),
+    path('userprofile/<int:i>',views.user_profile,name='userprofile'),
+    path('verify1',views.verify1,name='verify1'),
+    path('verify2',views.verify2,name='verify2'),
+    path('applicationpage/<int:i>',views.application_page,name='application_page'),
+    path('sendmsg',views.send_msg,name='sendmsg'),
+    path('savejob/<int:i>',views.save_job,name='savejob'),
+    path('filtercandidates/<int:i>',views.filter_candidates,name='filtercandidates')
+]
---
+++ cb_app/views.py
@@ -0,0 +1,274 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate,logout
+from .models import Jobs,UsersProfile,Applications,Messages,Saved_jobs
+# import vonage
+from django.contrib.auth.decorators import login_required
+
+# from django.http import FileResponse, Http404
+# from django.shortcuts import get_object_or_404
+from django.conf import settings
+# import os
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def courses(request):
+    return render(request, 'courses.html')
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
+
+def sign_up(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        if not User.objects.filter(username=username).exists():
+            new_user=User.objects.create_user(username=username,email=email,password=password)
+            new_user.save()
+            return redirect('login')
+        else:
+            return render(request,'auth/sign_up.html')
+    else:
+        return render(request, 'auth/sign_up.html')
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            return render(request, 'auth/login.html')
+
+    else:
+        return render(request, 'auth/login.html')
+
+def python(request):
+    return render(request, 'courses/python.html')
+
+
+#admin
+@login_required(login_url='adminlogin')
+def admin_index(request):
+    return render(request,'admin/admin_index.html')
+
+
+def admin_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if (user is not None) and user.is_staff:
+            login(request,user)
+            return redirect('adminindex')
+        else:
+            return render(request,'admin/login.html')
+    else:
+        return render(request,'admin/login.html')
+    
+@login_required(login_url='adminlogin')
+def jobs(request):
+    jobs=Jobs.objects.all()
+    if request.method=='POST':
+        job_title=request.POST['job_title']
+        company_name=request.POST['company_name']
+        location=request.POST['location']
+        mode=request.POST['mode']
+        description=request.POST['description']
+        experience_required=request.POST['experience_required']
+        skills_required=request.POST['skills_required']
+        starting_salary=request.POST['starting_salary']
+        ending_salary=request.POST['ending_salary']
+        languages=request.POST['languages']
+        # resume=request.FILES.get('resume')
+        new_job=Jobs(job_title=job_title,
+                     company_name=company_name,
+                     location=location,
+                     mode=mode,
+                     description=description,
+                     experience_required=experience_required,
+                     skills_required=skills_required,
+                     starting_salary=starting_salary,
+                     ending_salary=ending_salary,
+                     languages=languages,
+                     )
+        new_job.save()
+        return render(request,'admin/job_post.html',{'jobs':jobs})
+    else:
+        return render(request,'admin/job_post.html',{'jobs':jobs})
+
+ 
+def edit_profile(request):
+        current_user=request.user
+        if request.method=='POST':
+            fullname=request.POST['name']
+            photo=request.FILES.get('photo')
+            phone1=request.POST['phone1']
+            phone2=request.POST['phone2']
+            email=request.POST['email']
+            current_address=request.POST['current_address']
+            permanant_address=request.POST['permanant_address']
+            education=request.POST['education']
+            skills=request.POST['skills']
+            # Experience=request.POST['experience']
+            certifications=request.POST['certifications']
+            resume=request.FILES.get('resume')
+            user_profile=UsersProfile(fullname=fullname,
+                                    profile_user=current_user,
+                                    photo=photo,
+                                    email=email,
+                                    phone1=phone1,
+                                    phone2=phone2,
+                                    current_address=current_address,
+                                    permanant_address=permanant_address,
+                                    Education=education,
+                                    skills=skills,
+                                    certifications=certifications,
+                                    resume=resume,
+                                    )
+            user_profile.save()
+            # return render(request,'editProfile.html')
+            return redirect('profile')
+        else:
+            return render(request,'editProfile.html')
+        
+@login_required(login_url='login')
+def profile(request):
+    current_user=request.user
+    try:
+        profile=UsersProfile.objects.get(profile_user=current_user)
+        applied_jobs=Applications.objects.filter(candidate=current_user)
+        saved_jobs=Saved_jobs.objects.filter(candidate=current_user)
+    except:
+        profile=None
+        applied_jobs=None
+        saved_jobs=None
+    context={'profile':profile,'applied_jobs':applied_jobs,'saved_jobs':saved_jobs}
+    return render(request,'profile.html',context)
+
+@login_required(login_url='adminlogin')
+def users_list(request):
+    candidates=UsersProfile.objects.all()
+    return render(request,'admin/usersList.html',{'candidates':candidates})
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')
+
+@login_required(login_url='login')
+def jobs_available(request):
+    jobs=Jobs.objects.all()
+    current_user=request.user
+    applied_jobs=Applications.objects.filter(candidate=current_user)
+    print(len(applied_jobs))
+    return render(request,'jobs.html',{'jobs':jobs,'applied_jobs':applied_jobs})
+
+@login_required(login_url='/custom-login/')
+def apply_job(request,i):
+    job=Jobs.objects.get(id=i)
+    candidate=request.user
+    if Applications.objects.filter(candidate=candidate,job=job).exists():
+        return redirect("jobsavailable")
+    else:
+        new_applications=Applications(candidate=candidate,job=job)
+        new_applications.save()
+        print('successfully applied for the job')
+        return redirect('jobsavailable')
+
+
+    
+@login_required(login_url='adminlogin')
+def user_profile(request,i):
+    p=UsersProfile.objects.get(id=i)
+    u=p.profile_user
+    jobs=Applications.objects.filter(candidate=u)
+    return render(request,'user_profile.html',{'jobs':jobs,'profile':p})
+
+
+def verify1(request):
+    current_user=request.user
+    global profile
+    profile=UsersProfile.objects.get(profile_user=current_user)
+    phone=profile.phone1
+    global client
+    global verify
+    client = vonage.Client(key="479aaf01", secret="V3w3NMQNjnZqDKYS")
+    verify = vonage.Verify(client)
+
+    response = verify.start_verification(number=phone, brand="AcmeInc")
+    global REQUEST_ID
+    REQUEST_ID=response['request_id']
+
+    if response["status"] == "0":
+        print("Started verification request_id is %s" % (response["request_id"]))
+        return render(request,'verify.html')
+    else:
+        print("Error: %s" % response["error_text"])
+        return redirect('profile')
+    
+
+
+def verify2(request):
+    CODE=request.POST['code']
+
+    response = verify.check(REQUEST_ID, code=CODE)
+
+    if response["status"] == "0":
+        profile.phone1_verified=True
+        profile.save()
+        print("Verification successful, event_id is %s" % (response["event_id"]))
+        return redirect('profile')
+    else:
+        print("Error: %s" % response["error_text"])
+        msg='invalid otp'
+        return render(request,'verify.html',{'msg':msg})
+    
+# views.py
+
+
+def application_page(request,i):
+    job=Jobs.objects.get(id=i)
+    return render(request,'applyjob.html',{'job':job})
+
+
+def send_msg(request):
+    first_name=request.POST['fName']
+    last_name=request.POST['lName']
+    email=request.POST['email']
+    mobile=request.POST['phone']
+    message=request.POST['message']
+    new_message=Messages(first_name=first_name,
+                         last_name=last_name,
+                         email=email,
+                         mobile=mobile,
+                         message=message)
+    new_message.save()
+    print('message sent')
+    msg='Thank you we will reach you soon....'
+    return render(request,'contact_us.html',{'msg':msg})
+
+def save_job(request,i):
+    job=Jobs.objects.get(id=i)
+    candidate=request.user
+    if Saved_jobs.objects.filter(candidate=candidate,job=job).exists():
+        return redirect("jobsavailable")
+    else:
+        new_job=Saved_jobs(candidate=candidate,job=job)
+        new_job.save()
+        print('successfully applied for the job')
+        return redirect('jobsavailable')
+     
+def filter_candidates(request,i):
+    job=Jobs.objects.get(id=i)
+    applied_jobs=Applications.objects.filter(job=job)
+    applied_users=[c.candidate for c in applied_jobs]
+    user_profiles = UsersProfile.objects.filter(profile_user__in=applied_users)
+    return render(request,'filtered_candidates.html',{'applied_candidates':user_profiles})
