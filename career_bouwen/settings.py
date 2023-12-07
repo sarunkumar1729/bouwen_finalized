731b08cc60957563f828db2cd908446cc9b62210
sarunkumar1729 <sarunkumarvv1993@gmail.com>
2023-12-07 17:12:57 +0530

    fifteenth commit

--- career_bouwen/settings.py
+++ career_bouwen/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zm&xpwo$!3tku%&&pi0_f=mapmnigmqmct%e&jh&mru$es5-)w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.careerbouwen.com']
 
 
 # Application definition
